ef82c7a8aab5bf9cc65c373f4e9f54005815dcf0
Kashyap136 <HARSHKASHYAP664@GMAIL.COM>
2026-05-17 00:41:13 +0530

    Add edge runtime to write-editor-page

--- src/app/write-editor-page/page.tsx
+++ src/app/write-editor-page/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AppLayout from '@/components/AppLayout';
 import WriteEditorClient from './components/WriteEditorClient';
 
+export const runtime = 'edge';
 export const dynamic = 'force-dynamic';
 
 export default function WriteEditorPage() {
